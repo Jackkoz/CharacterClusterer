17f43accbf6d365f5509356a6d33790afa70f211
Jacek Koziński <jacek.koz@gmail.com>
2015-04-30 10:48:20 +0200

    Reading from command line, writing to file

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,7 +4,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.RasterFormatException;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,91 +12,37 @@ public class Main
 {
     public static void main(String[] args) throws IOException
     {
-        long start = System.currentTimeMillis();
-
         ArrayList<Character> characters = new ArrayList<>();
-        File[] files = new File("C:\\Users\\Jacek\\Documents\\Studia\\III\\SUS\\myapp\\data").listFiles();
-        for (File f : files)
+        for (File f : new File(args[1]).listFiles())
         {
             try
             {
                 characters.add(new Character(ImageIO.read(f), f.getName()));
             }
-            catch (RasterFormatException | EmptyImageException e)
-            {
-                System.out.println(f.getName());
-                System.out.println(e.getMessage());
-            }
+            catch (RasterFormatException | EmptyImageException ignored) {}
         }
 
         DBScan clusterer = new DBScan(10, 1);
-        int clustered = 0;
-        int i = 0;
 
         List<List<Character>> clusters = clusterer.cluster(characters);
 
-        long end = System.currentTimeMillis();
-        long elapsed = end - start;
-
-        File root = new File("C:\\Users\\Jacek\\Desktop\\sus\\");
-
-        if (root.exists())
-        {
-            Files.walk(root.toPath()).
-                    sorted((a, b) -> b.compareTo(a)). // reverse; files before dirs
-                    forEach(p -> {
-                try
-                {
-                    if (! p.toFile().isDirectory())
-                    {
-                        Files.delete(p);
-                    }
-                } catch (IOException e)
-                { /* ... */ }
-            });
-        }
-
-        root.mkdir();
+        PrintWriter output = new PrintWriter(args[2], "UTF-8");
 
         for (List<Character> c : clusters)
         {
-            i++;
-            File f = new File(root.getPath() + "\\" + i);
-            if (!f.exists())
-            {
-                f.mkdir();
-            }
-//            System.out.println("*******");
-//            System.out.println("Cluster size: " + c.size());
-            clustered += c.size();
             for (Character chr : c)
             {
-//                System.out.println(chr.fileName);
-                File outputfile = new File(f.getPath() + "\\" +  chr.fileName);
-                ImageIO.write(chr.img, "png", outputfile);
+                output.print(chr.fileName + " ");
             }
+            output.println();
         }
 
-        File noiseDir = new File(root.getPath() + "\\noise");
-        if (!noiseDir.exists())
-            noiseDir.mkdir();
-
-        for (Character c : clusterer.noiseSet)
+        for (Character chr : clusterer.noiseSet)
         {
-            File outputfile = new File(noiseDir.getPath() + "\\" + c.fileName);
-            ImageIO.write(c.img, "png", outputfile);
+            output.print(chr.fileName + " ");
+            output.println();
         }
 
-        System.out.println();
-        System.out.println("Clusters: " + clusters.size());
-        System.out.println("Total in clusters: " + clustered);
-        System.out.print("Cluster sizes: ");
-        for (List c : clusters)
-            System.out.print(c.size() + " ");
-        System.out.println();
-
-        long minutes = Math.floorDiv(elapsed, 1000 * 60);
-        long seconds = elapsed/1000 - 60 * minutes;
-        System.out.println("\nTime: " + minutes + ":" + seconds);
+        output.close();
     }
 }
